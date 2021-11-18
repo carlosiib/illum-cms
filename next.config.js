7ad8a6df68e2cf30299b3efe8cd2530f9f642abe
carlosiib <ntic.carlos@gmail.com>
2021-11-18 16:21:12 -0700

    next config target

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const { locales } = require('./lib/_locales')
 
 module.exports = {
+  target: "serverless",
   i18n: {
     defaultLocale: locales.find((locale) => locale.default).value,
     locales: locales.map((locale) => locale.value)
