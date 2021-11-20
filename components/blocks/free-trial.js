0c0440fe50456ad5f9ceab84747d450b55e0f410
carlosiib <ntic.carlos@gmail.com>
2021-11-20 14:54:22 -0700

    Update schema prop names free trial

--- components/blocks/free-trial.js
+++ components/blocks/free-trial.js
@@ -2,19 +2,19 @@ import Link from 'next/link'
 import { Heading, Box } from '@chakra-ui/layout'
 
 export default function FreeTrial({
-  title: FTTitle,
-  subTitle: FTSubtitle,
-  hrefLabel: FTLabel,
-  hrefLink: FTLink }) {
+  freeTrialTitle,
+  freeTrialSubtitle,
+  freeTrialHrefLabel,
+  freeTrialHrefLink }) {
 
   // Rename prop otherwise duplicated prop name error -> CMS schema names  
 
   return (
     <Box as="section" maxW="7xl" mx="auto" className="b-2" my={12} className="free-trial-container">
-      <Heading as="h4" mb={6}>{FTTitle}</Heading>
-      <p>{FTSubtitle}</p>
-      <Link href={FTLink}>
-        <a target="_blank" rel="noreferrer" className="anchor-link" >{FTLabel}</a>
+      <Heading as="h4" mb={6}>{freeTrialTitle}</Heading>
+      <p>{freeTrialSubtitle}</p>
+      <Link href={freeTrialHrefLink}>
+        <a target="_blank" rel="noreferrer" className="anchor-link" >{freeTrialHrefLabel}</a>
       </Link>
     </Box>
   )
