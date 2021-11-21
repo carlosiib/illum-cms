7d95869ef57c415f93fa692ec9bb672f25075881
carlosiib <ntic.carlos@gmail.com>
2021-11-20 19:14:32 -0700

    Add react intersection observer

--- components/blocks/how-does-it-work.js
+++ components/blocks/how-does-it-work.js
@@ -1,6 +1,33 @@
+import { useEffect } from "react"
 import { Box, Heading, Flex } from "@chakra-ui/layout"
+import { useInView } from 'react-intersection-observer'
+import { motion, useAnimation } from 'framer-motion'
 
 export default function HowDoesItWork({ title, mainImage, howDoesItWorkContents }) {
+
+  // Intersection observer Hook for animations
+  const { ref: refContainer, inView: inViewContainer } = useInView()
+  const { ref: refImg, inView: inViewImg } = useInView()
+  const imageAnimation = useAnimation()
+
+  const MotionBox = motion(Box)
+
+  useEffect(() => {
+    console.log("inview SKEW CONTAINER")
+
+    //x: (options.step * 1) % 2 == 0 ? "100%" : "0%",
+
+    imageAnimation.start({
+      x: "55%",
+      opacity: 1,
+      transition: {
+        ease: [0.17, 0.67, 0.83, 0.67],
+        type: "spring",
+        bounceDamping: .25
+      }
+    })
+
+  }, [inViewContainer])
 
 
   if (!howDoesItWorkContents) return null
@@ -9,8 +36,9 @@ export default function HowDoesItWork({ title, mainImage, howDoesItWorkContents 
   return (
     <Box maxW="7xl" mx="auto" py={12} className="hdw-container">
       <Heading as="h5" mb={14} textAlign={'center'}>{title}</Heading>
+
       <Box className="hdw-content">
-        <Box position={'sticky'} top={'25%'} zIndex={'10'}>
+        <MotionBox animate={imageAnimation} position={'sticky'} top={'25%'} zIndex={'10'}>
           <img
             src={mainImage.url}
             loading="lazy"
@@ -23,9 +51,9 @@ export default function HowDoesItWork({ title, mainImage, howDoesItWorkContents 
             alt="Grader console dashboard"
             className="secondary-img"
           />
-        </Box>
+        </MotionBox>
 
-        <Box>
+        <Box ref={refContainer}>
           {
             howDoesItWorkContents &&
             howDoesItWorkContents.map((content, idx) =>
