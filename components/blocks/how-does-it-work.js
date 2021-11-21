62cd50d953c6612daa9cc1cdd073e19e3d525398
carlosiib <ntic.carlos@gmail.com>
2021-11-20 21:01:31 -0700

    Add skew content

--- components/blocks/how-does-it-work.js
+++ components/blocks/how-does-it-work.js
@@ -1,44 +1,65 @@
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 import { Box, Heading, Flex } from "@chakra-ui/layout"
 import { useInView } from 'react-intersection-observer'
 import { motion, useAnimation } from 'framer-motion'
 
 export default function HowDoesItWork({ title, mainImage, howDoesItWorkContents }) {
 
-  // Intersection observer Hook for animations
-  const { ref: refContainer, inView: inViewContainer } = useInView()
-  const { ref: refImg, inView: inViewImg } = useInView()
+  const config = {
+    change: false,
+    step: {}
+  }
+
+  const [animateStep, setAnimateStep] = useState(config)
   const imageAnimation = useAnimation()
 
-  const MotionBox = motion(Box)
 
   useEffect(() => {
-    console.log("inview SKEW CONTAINER")
+    const { change, step } = animateStep
 
-    //x: (options.step * 1) % 2 == 0 ? "100%" : "0%",
+    if (change) {
+      imageAnimation.start({
+        x: (step * 1) % 2 == 0 ? "50%" : "0%",
+        opacity: 1,
+        transition: {
+          ease: [0.17, 0.67, 0.83, 0.67],
+          type: "spring",
+          bounceDamping: .25
+        }
+      })
 
-    imageAnimation.start({
-      x: "55%",
-      opacity: 1,
-      transition: {
-        ease: [0.17, 0.67, 0.83, 0.67],
-        type: "spring",
-        bounceDamping: .25
+      const changedPathImg = document.querySelector(".secondary-img")
+      if (step == "1") {
+        changedPathImg.src = `${howDoesItWorkContents[0].secondaryImage.url}`
       }
-    })
+      if (step == "2") {
+        changedPathImg.src = `${howDoesItWorkContents[1].secondaryImage.url}`
+      }
+      if (step == "3") {
+        changedPathImg.src = `${howDoesItWorkContents[2].secondaryImage.url}`
+      }
+      if (step == "4") {
+        changedPathImg.src = `${howDoesItWorkContents[3].secondaryImage.url}`
+      }
+    }
 
-  }, [inViewContainer])
+
+  }, [animateStep, imageAnimation])
+
+
+  function handleAnimation(step) {
+    setAnimateStep({ change: true, step })
+  }
 
 
   if (!howDoesItWorkContents) return null
-
 
   return (
     <Box maxW="7xl" mx="auto" py={12} className="hdw-container">
       <Heading as="h5" mb={14} textAlign={'center'}>{title}</Heading>
 
       <Box className="hdw-content">
-        <MotionBox animate={imageAnimation} position={'sticky'} top={'25%'} zIndex={'10'}>
+        <motion.div animate={imageAnimation} className="skew-container">
           <img
             src={mainImage.url}
             loading="lazy"
@@ -51,48 +72,84 @@ export default function HowDoesItWork({ title, mainImage, howDoesItWorkContents 
             alt="Grader console dashboard"
             className="secondary-img"
           />
-        </MotionBox>
+        </motion.div>
 
-        <Box ref={refContainer}>
-          {
-            howDoesItWorkContents &&
-            howDoesItWorkContents.map((content, idx) =>
-            (
-              <div key={content.title}>
-                {
-                  idx === 0 ?
-                    (
-                      <Box color={'red'} className="first-content hwd-description">
-                        <Heading as="h6">{content.title}</Heading>
-                        <p>{content.subtitle}</p>
-                      </Box>
-                    ) :
-                    idx % 2 !== 0 ?
-                      (
-                        <Box className="left-description hwd-description">
-                          <Heading color={'orange'} as="h6">{content.title}</Heading>
-                          <p>{content.subtitle}</p>
-                        </Box>
-                      ) :
-                      (
-                        <Flex justifyContent={'flex-end'}>
-                          <Box className="right-description hwd-description">
-                            <Heading color={'yellow'} as="h6">{content.title}</Heading>
-                            <p>{content.subtitle}</p>
-                          </Box>
-                        </Flex >
-
-                      )
-                }
-
-              </div>
-            ))
-          }
-        </Box>
+        {
+          howDoesItWorkContents &&
+          (
+            <div >
+              <SkewContent
+                step={"1"}
+                content={howDoesItWorkContents[0]}
+                downAnimation={(direction) => handleAnimation(direction)}
+              />
+              <SkewContent
+                step={"2"}
+                content={howDoesItWorkContents[1]}
+                downAnimation={(direction) => handleAnimation(direction)}
+              />
+              <SkewContent
+                step={"3"}
+                content={howDoesItWorkContents[2]}
+                downAnimation={(direction) => handleAnimation(direction)}
+              />
+              <SkewContent
+                step={"4"}
+                content={howDoesItWorkContents[3]}
+                downAnimation={(direction) => handleAnimation(direction)}
+              />
+            </div>
+          )
+        }
       </Box>
-
     </Box>
   )
 }
 
+function SkewContent({ content, step, downAnimation }) {
+  // Intersection observer Hook for animations
+  const { ref, inView } = useInView({ threshold: 0.8 })
+
+  useEffect(() => {
+    if (inView) {
+      downAnimation(step)
+    }
+  }, [inView, step])
+
+  return (
+    <>
+
+      {step === "1" && (
+        <Box ref={ref} color={'red'} className="first-content hwd-description">
+          <Heading as="h6">{content.title}</Heading>
+          <p>{content.subtitle}</p>
+        </Box>
+      )}
+
+      {step === "2" && (
+        <Box ref={ref} color={'orange'} className="left-description hwd-description">
+          <Heading as="h6">{content.title}</Heading>
+          <p>{content.subtitle}</p>
+        </Box>
+      )}
+
+      {step === "3" && (
+        <Flex ref={ref} justifyContent={'flex-end'}>
+          <Box color={'yellow'} className="right-description hwd-description">
+            <Heading as="h6">{content.title}</Heading>
+            <p>{content.subtitle}</p>
+          </Box>
+        </Flex>
+      )}
+
+      {step === "4" && (
+        <Box ref={ref} color={'orange'} className="left-description hwd-description">
+          <Heading as="h6">{content.title}</Heading>
+          <p>{content.subtitle}</p>
+        </Box>
+      )}
+
+    </>
+  )
+}
 
